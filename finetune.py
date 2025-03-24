506a42ea3478a3acdc9d2d166cfe0930c7e5f8a3
JacksonBurns <jwburns@mit.edu>
2025-03-24 14:34:58 -0400

    finetuning nb

--- finetune.py
+++ finetune.py
@@ -6,7 +6,9 @@ zarr>=3 so two separate python environments are needed
 """
 from pathlib import Path
 import sys
+import datetime
 import warnings
+import json
 
 import torch
 from mordred import Calculator, descriptors
@@ -15,6 +17,7 @@ from rdkit.Chem import MolFromSmiles
 import polaris as po
 from polaris.utils.types import TargetType
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.neural_network import MLPClassifier, MLPRegressor
 
 from models import fastpropFoundation
 
@@ -41,9 +44,17 @@ if __name__ == "__main__":
     except:
         print("usage: python pretrain.py PRETRAINED_CHECKPOINT_PATH OUTPUT_DIR")
         exit(1)
+    if not output_dir.exists():
+        output_dir.mkdir()
+    output_file = open(output_dir / "finetune_results.md", "w")
+    output_file.write(f"""# `fastprop` Finetuning Results
+timestamp: {datetime.datetime.now()}
+pretrained model: {ckpt_path}
+""")
+    performance_dict = {}
     polaris_benchmarks = [
-        # "polaris/pkis2-lok-slk-cls-v2",
-        # "polaris/pkis2-ret-wt-cls-v2",
+        # "polaris/pkis2-lok-slk-cls-v2",  <-- need to add support for multitask binary classification
+        "polaris/pkis2-ret-wt-cls-v2",
         # "polaris/pkis2-ret-wt-reg-v2",
         # "polaris/pkis2-kit-wt-cls-v2",
         # "polaris/pkis2-kit-wt-reg-v2",
@@ -54,17 +65,16 @@ if __name__ == "__main__":
         "biogen/adme-fang-perm-reg-v1",
         "biogen/adme-fang-rclint-reg-v1",
         "biogen/adme-fang-hclint-reg-v1",
-        "biogen/adme-fang-reg-v1",
         # "tdcommons/lipophilicity-astrazeneca",
         # "tdcommons/ppbr-az",
         # "tdcommons/clearance-hepatocyte-az",
         # "tdcommons/cyp2d6-substrate-carbonmangels",
         # "tdcommons/half-life-obach",
         # "tdcommons/cyp2c9-substrate-carbonmangels",
-        # "tdcommons/clearance-microsome-az",
-        # "tdcommons/dili",
-        # "tdcommons/bioavailability-ma",
-        # "tdcommons/vdss-lombardo",
+        "tdcommons/clearance-microsome-az",
+        "tdcommons/dili",
+        "tdcommons/bioavailability-ma",
+        "tdcommons/vdss-lombardo",
         # "tdcommons/cyp3a4-substrate-carbonmangels",
         # "tdcommons/pgp-broccatelli",
         # "tdcommons/caco2-wang",
@@ -85,30 +95,45 @@ if __name__ == "__main__":
         train_df, test_df = train.as_dataframe(), test.as_dataframe()
         encodings = get_pretrained_encodings(train_df[smiles_col], ckpt_path)
         test_encodings = get_pretrained_encodings(test_df[smiles_col], ckpt_path)
-        targets = train_df[target_cols].to_numpy()
+        targets = train_df[target_cols]
+        targets = targets.fillna(targets.mean(axis=0)).to_numpy()
         if targets.shape[1] == 1:
             targets = targets.flatten()
         # fit a random forest model
         task_type = benchmark.target_types[target_cols[0]]
         if task_type == TargetType.CLASSIFICATION:
-            rf = RandomForestClassifier()
-            rf.fit(encodings, targets)
-            predictions = rf.predict_proba(test_encodings)
+            model = MLPClassifier()
+            model.fit(encodings, targets)
+            predictions = model.predict_proba(test_encodings)
             predictions = predictions[:, 1].flatten()
         elif task_type == TargetType.REGRESSION:
-            rf = RandomForestRegressor()
-            rf.fit(encodings, targets)
-            predictions = rf.predict(test_encodings)
+            model = MLPRegressor()
+            model.fit(encodings, targets)
+            predictions = model.predict(test_encodings)
         else:
             print(f"Unknown task type {task_type}")
             exit(1)
         # run inference
-        # prepare the predictions in the format polaris expects (based on # targets, reg. or class.)
+        # prepare the predictions in the format polaris expects
         if task_type == TargetType.CLASSIFICATION:
             results = benchmark.evaluate(predictions > 0.5, predictions)
         elif task_type == TargetType.REGRESSION:
-            if len(target_cols) > 1:  # only regression (?)
-                predictions = {t: predictions[:, i] for i, t in enumerate(target_cols)}
+            # if len(target_cols) > 1:  # if there were multitask
+            #     predictions = {t: predictions[:, i] for i, t in enumerate(target_cols)}
             results = benchmark.evaluate(predictions)
-        print(results.results)
-        # save results, plot them
+        output_file.write(f"""
+## `{benchmark_name}`
+
+{results.results.to_markdown()}
+
+""")
+        performance = results.results.query(f"Metric == '{benchmark.main_metric.label}'")['Score'].values[0]
+        performance_dict[benchmark_name] = performance
+    
+    output_file.write(f"""
+# Summary
+
+```
+results_dict = {json.dumps(performance_dict, indent=4)}
+```
+""")
