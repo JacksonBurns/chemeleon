add variable depth decoder, fix feature calculation bug

features.py
@@ -12,7 +12,7 @@ import warnings
 import numpy as np
 from rdkit import rdBase
 from mordred import Calculator, descriptors
-from rdkit.Chem import MolFromSmiles
+from rdkit.Chem import MolFromSmiles, RemoveHs
 from tqdm import tqdm
 import zarr
 
@@ -21,7 +21,15 @@ warnings.filterwarnings('ignore', category=FutureWarning)
 
 # convert to mols, filtering invalid
 def _f(smi):
-    return MolFromSmiles(smi) is not None
+    mol = MolFromSmiles(smi)
+    if mol is None:
+        return False
+    try:
+        _ = RemoveHs(mol, updateExplicitCount=True)
+    except:
+        print("Skipping mol {smi} - failed RemoveHs")
+        return False
+    return True
 
 # convert to mols
 def _s(smi):
@@ -72,16 +80,18 @@ if __name__ == "__main__":
     print(f"Number of rows per chunk: {chunk_rows}")
 
     # Create the dataset with compression and concurrency settings
-    z = zarr.create_array(
-        store=out_file,
-        shape=shape,
-        chunks=chunk_shape,
-        dtype=dtype,
-        compressors=None,  # disable compression
-        fill_value=np.nan,
-    )
+    # z = zarr.create_array(
+    #     store=out_file,
+    #     shape=shape,
+    #     chunks=chunk_shape,
+    #     dtype=dtype,
+    #     compressors=None,  # disable compression
+    #     fill_value=np.nan,
+    # )
+    z = zarr.open_array(out_file)
 
-    i = 0
+    # restarting from previous - otherwise start at i = 0
+    i = 7_680_096
     with tqdm(total=n_mols, desc="Calculating features") as pbar:
         while i < n_mols:
             mols = list(p.map(_s, smiles[i:i+chunk_rows], chunksize=chunk_rows // 64))

models.py
@@ -35,6 +35,7 @@ class fastpropFoundation(LightningModule):
         num_features: int = 1613,
         winsorization_factor=None,
         hidden_sizes: Tuple[int, ...] = (1024,),
+        decoder_sizes: Tuple[int, ...] = (256,),
         encoding_size: int = 256,
         learning_rate: float = 0.001,
         masking_ratio: float = 0.15,
@@ -61,7 +62,14 @@ class fastpropFoundation(LightningModule):
         modules.append(torch.nn.Linear(hidden_sizes[-1], encoding_size))
         self.encoder = torch.nn.Sequential(*modules)
         # decoder
-        self.decoder = torch.nn.Linear(encoding_size, num_features)
+        modules = []
+        modules.append(torch.nn.Linear(encoding_size, decoder_sizes[0]))
+        modules.append(torch.nn.LeakyReLU())
+        for i in range(len(decoder_sizes)-1):
+            modules.append(torch.nn.Linear(decoder_sizes[i], decoder_sizes[i+1]))
+            modules.append(torch.nn.LeakyReLU())
+        modules.append(torch.nn.Linear(decoder_sizes[-1], num_features))
+        self.decoder = torch.nn.Sequential(*modules)
 
         self.save_hyperparameters()
     

pretrain.py
@@ -27,12 +27,14 @@ from utils.torchford import Welford
 
 
 # architecture
-ENCODING_SIZE = 4_096
-HIDDEN_SIZES = tuple([ENCODING_SIZE] * 3)
 EMBEDDING_DIM = 8
-# (1129, 790, 553, 387, 270, 189, 132, 92)  # contiually shrink by 30%
-# tuple(reversed(range(ENCODING_SIZE-440, 1613, -440)))  # overcomplete 
-# tuple(reversed(range(ENCODING_SIZE + 42, 1613, 42)))  # (1500, 1300, 1100, 900, 700, 500, 300, 100)
+ENCODING_SIZE = 128
+HIDDEN_SIZES = (8192, 4096, 2048, 1024, 512, 256)
+DECODER_SIZES = (128, 128)
+# output/pubchem1MM_MDAE_PLR_15mask_8embedding_4096x3layers_4096encoding
+# tuple([ENCODING_SIZE] * 3) encoding 4096
+# output/pubchem1MM_MDAE_15mask_64encoding_shrinkpercent30
+# (1129, 790, 553, 387, 270, 189, 132, 92)  # contiually shrink by 30% encoding 64
 
 # training configuration
 LEARNING_RATE = 1e-5
@@ -99,6 +101,7 @@ if __name__ == "__main__":
         num_features=n_features,
         winsorization_factor=6,
         hidden_sizes=HIDDEN_SIZES,
+        decoder_sizes=DECODER_SIZES,
         encoding_size=ENCODING_SIZE,
         learning_rate=LEARNING_RATE,
         masking_ratio=0.15,
@@ -138,6 +141,5 @@ if __name__ == "__main__":
     ckpt_path = trainer.checkpoint_callback.best_model_path
     print(f"Reloading best model from checkpoint file: {ckpt_path}")
     model = model.__class__.load_from_checkpoint(ckpt_path)
-    trainer = Trainer(devices=1, logger=tensorboard_logger)
     trainer.test(model, test_dataloader)
     torch.save(model, output_dir / "best.pt")