c7a0555b79ac8c2151ee04d510710102ff314575
JacksonBurns <jwburns@mit.edu>
2025-04-17 10:21:27 -0400

    add foundation chemprop training code

--- chemprop_foundation.py
+++ chemprop_foundation.py
@@ -31,6 +31,7 @@ class ChemPropZarrDataset(torch.utils.data.Dataset):
         self.smiles = smiles
         self.len = len(smiles)
         self.z = zarr.open_array(zarr_store)
+        assert self.z.shape[0] == len(smiles), "Mismatched smiles and feature sizes"
         self.molgraph_generator = SimpleMoleculeMolGraphFeaturizer()
 
     def __len__(self):
@@ -38,7 +39,7 @@ class ChemPropZarrDataset(torch.utils.data.Dataset):
     
     def __getitem__(self, idx: int):
         mg = self.molgraph_generator(MolFromSmiles(self.smiles[idx]))
-        return Datum(mg, None, None, self.z[idx, :], None, None, None)     
+        return Datum(mg, None, None, self.z[idx, :], 1.0, None, None)     
 
 
 class MaskedDescriptorsMPNN(MPNN):
@@ -59,6 +60,18 @@ class MaskedDescriptorsMPNN(MPNN):
         self.register_buffer("feature_vars", feature_vars)
         self.winsorization = WinsorizeStdevN(winsorization_factor)
 
+    def validation_step(self, batch, batch_idx = 0):
+        bmg, V_d, X_d, targets, weights, lt_mask, gt_mask = batch
+        targets = standard_scale(targets, self.feature_means, self.feature_vars)
+        targets = self.winsorization(targets)
+        return super().validation_step((bmg, V_d, X_d, targets, weights, lt_mask, gt_mask), batch_idx)
+
+    def test_step(self, batch, batch_idx = 0):
+        bmg, V_d, X_d, targets, weights, lt_mask, gt_mask = batch
+        targets = standard_scale(targets, self.feature_means, self.feature_vars)
+        targets = self.winsorization(targets)
+        return super().test_step((bmg, V_d, X_d, targets, weights, lt_mask, gt_mask), batch_idx)
+
     def training_step(self, batch, batch_idx):
         # overrides parent method to ranomly mask (evaluate only masked descriptors) during training
         batch_size = self.get_batch_size(batch)
@@ -74,30 +87,106 @@ class MaskedDescriptorsMPNN(MPNN):
 
 
 if __name__ == "__main__":
-    from chemprop.nn import MeanAggregation, BondMessagePassing
+    import sys
+    from pathlib import Path
     
-    zarr_store = "/datag/pubchem_1MM.zarr"
+    from tqdm import tqdm
+    from chemprop.nn import MeanAggregation, BondMessagePassing, RegressionFFN, metrics
+    from chemprop.data import build_dataloader
+    from lightning.pytorch.utilities import rank_zero_info
+    from lightning.pytorch import Trainer
+    from lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint
+    from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+    from lightning.pytorch.loggers import TensorBoardLogger
     
-    # get the smiles corresponding to the rows of this zarr arr...
-    # /home/jwburns/fastprop_foundation/pubchem_1MM.smiles
-    # but apply the filtering steps used in the features.py script
     
-    dataset = ChemPropZarrDataset(
-        None,
-        zarr_store,
-    )
+    BATCH_SIZE = 128
+    NUM_EPOCHS = 500
+    PATIENCE = 50
+    HIDDEN_SIZE = 2_048
+    DEPTH = 6
+    
+    try:
+        training_store = Path(sys.argv[1])
+        output_dir = Path(sys.argv[2])
+        smiles_file = Path(sys.argv[3])
+    except:
+        print("usage: python chemprop_foundation.py TRAINING_STORE OUTPUT_DIR SMILES_FILE")
+        exit(1)
+        
+    z = zarr.open_array(training_store, mode='r')
+    n_features = z.shape[1]
+    del z
 
-    mdm = MaskedDescriptorsMPNN(
-        BondMessagePassing(),
+    with open(smiles_file, "r") as file:
+        smiles = [i.strip() for i in tqdm(file.readlines(), "Reading SMILES")]
+    
+    
+    # lightning training code from other script    
+    dataset = ChemPropZarrDataset(
+        smiles,
+        training_store,
+    )
+    gen = torch.Generator().manual_seed(1701)
+    train_dset, val_dset, test_dset = torch.utils.data.random_split(dataset, [0.7, 0.2, 0.1], gen)
+    train_dataloader = build_dataloader(train_dset, num_workers=4, batch_size=BATCH_SIZE, shuffle=True, persistent_workers=True)
+    val_dataloader = build_dataloader(val_dset, num_workers=4, batch_size=BATCH_SIZE, shuffle=False, persistent_workers=True)
+    test_dataloader = build_dataloader(test_dset, num_workers=4, batch_size=BATCH_SIZE, shuffle=False, persistent_workers=True)
+
+    cached_means_fpath = f"feature_means_cached_{training_store.stem}.pt"
+    cached_vars_fpath = f"feature_vars_cached_{training_store.stem}.pt"
+    feature_means = torch.load(cached_means_fpath, weights_only=True, map_location="cpu")
+    feature_vars = torch.load(cached_vars_fpath, weights_only=True, map_location="cpu")
+
+    
+    model = MaskedDescriptorsMPNN(
+        BondMessagePassing(d_h=HIDDEN_SIZE, depth=DEPTH),
         MeanAggregation(),
-        predictor=None,
-        metrics=[],
+        predictor=RegressionFFN(
+            n_tasks=n_features,
+            input_dim=HIDDEN_SIZE,
+            hidden_dim=1_024,
+        ),
+        metrics=[metrics.MSE()],
         masking_ratio=0.15,
-        feature_means=None,
-        feature_vars=None,
+        feature_means=feature_means,
+        feature_vars=feature_vars,
         winsorization_factor=6,
     )
-    
-    # lightning training code from other script
-    ...
-    train_dataset, val_dataset, test_dataset = None, None, None
+    rank_zero_info(model)
+
+    tensorboard_logger = TensorBoardLogger(
+        output_dir,
+        name="tensorboard_logs",
+        default_hp_metric=False,
+    )
+    callbacks = [
+        EarlyStopping(
+            monitor="val_loss",
+            mode="min",
+            verbose=False,
+            patience=PATIENCE,
+        ),
+        ModelCheckpoint(
+            monitor="val_loss",
+            save_top_k=2,
+            mode="min",
+            dirpath=output_dir / "checkpoints",
+        ),
+    ]
+    callbacks[1].STARTING_VERSION = 0
+    trainer = Trainer(
+        max_epochs=NUM_EPOCHS,
+        logger=tensorboard_logger,
+        log_every_n_steps=1,
+        enable_checkpointing=True,
+        check_val_every_n_epoch=1,
+        callbacks=callbacks,
+    )
+    trainer.fit(model, train_dataloader, val_dataloader)
+    ckpt_path = trainer.checkpoint_callback.best_model_path
+    print(f"Reloading best model from checkpoint file: {ckpt_path}")
+    model = model.__class__.load_from_checkpoint(ckpt_path, map_location="cpu")
+    trainer.test(model, test_dataloader)
+    torch.save(model, output_dir / "best.pt")
+
