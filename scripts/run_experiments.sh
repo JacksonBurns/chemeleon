7175e66173fd12a911324ebad456ea559e20956a
Akshat Zalte <akshatszalte@gmail.com>
2025-04-24 11:35:31 -0400

    modified the bash script

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -8,3 +8,6 @@ cd ../rf_fingerprint
 python evaluate.py output/all
 cd ../molformer_transformer
 python evaluate.py output/all
+cd ../pca_mlp
+python evaluate.py output/direct_all --gpu --pca-method on-the-fly --explained-variance 0.95
+python evaluate.py output/pretrained_all --gpu --pca-method pretrained --pca-model-path /home/jwburns/fastprop_foundation/models/pca_mlp/output/foundation_pca.pt
