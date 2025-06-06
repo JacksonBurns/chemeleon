57383b1db7d3d1a90f98efe335a3bf68a1a2d9bc
Akshat Zalte <akshatszalte@gmail.com>
2025-06-05 21:01:49 -0400

    Adding direct PCA results

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -29,6 +29,15 @@ mkdir -p output
 BENCHMARK_SET=moleculeace python evaluate.py output/moleculeace
 BENCHMARK_SET=polaris python evaluate.py output/polaris
 
-# cd ../pca_mlp
-# python evaluate.py output/direct_all --gpu --pca-method on-the-fly --explained-variance 0.95
-# python evaluate.py output/pretrained_all --gpu --pca-method pretrained --pca-model-path /home/jwburns/fastprop_foundation/models/pca_mlp/output/foundation_pca.pt
+cd ../rf_mordred
+mkdir -p output
+BENCHMARK_SET=moleculeace python evaluate.py output/moleculeace
+BENCHMARK_SET=polaris python evaluate.py output/polaris
+
+cd ../pca_mlp
+mkdir -p output
+BENCHMARK_SET=moleculeace python evaluate.py output/direct_moleculeace --gpu --pca-method on-the-fly --explained-variance 0.95
+BENCHMARK_SET=polaris python evaluate.py output/direct_polaris --gpu --pca-method on-the-fly --explained-variance 0.95
+python fit_pca.py /path/to/training_data.zarr  prefitted_pca_model.pt
+BENCHMARK_SET=moleculeace python evaluate.py output/direct_moleculeace --gpu --pca-method pretrained --pca-model-path prefitted_pca_model.pt
+BENCHMARK_SET=polaris python evaluate.py output/direct_polaris --gpu --pca-method pretrained --pca-model-path prefitted_pca_model.pt
