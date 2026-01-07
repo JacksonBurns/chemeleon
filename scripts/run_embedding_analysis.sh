e8dd5fd2756fea97e9834a361a5f9f588f636fc7
Akshat Zalte <akshatszalte@gmail.com>
2026-01-06 23:21:56 -0500

    correcting the bash script

--- scripts/run_embedding_analysis.sh
+++ scripts/run_embedding_analysis.sh
@@ -10,22 +10,22 @@ CONFIG_DIR="${EXPERIMENT_ROOT_DIR}/config"
 
 
 # 01 preprocess data
-python ${SCRIPT_DIR}/01_preprocess_data.py --n_jobs $N_JOBS
+# python ${SCRIPT_DIR}/01_preprocess_data.py --n_jobs $N_JOBS
 
 # 02 run clustering/splitting on pre-selected endpoints only
-python ${SCRIPT_DIR}/02_assign_groups.py --endpoint "${CONFIG_DIR}/endpoints.yaml" --n_groups 5 --n_jobs $N_JOBS
+# python ${SCRIPT_DIR}/02_assign_groups.py --endpoint "${CONFIG_DIR}/endpoints.yaml" --n_groups 5 --n_jobs $N_JOBS
 
 # 03 calculate descriptors
-python ${SCRIPT_DIR}/03_calc_descriptors.py --endpoint "${CONFIG_DIR}/endpoints.yaml" --n_jobs $N_JOBS --descriptor morgan
-python ${SCRIPT_DIR}/03_calc_descriptors.py --endpoint "${CONFIG_DIR}/endpoints.yaml" --n_jobs $N_JOBS --descriptor morgan_count
-python ${SCRIPT_DIR}/03_calc_descriptors.py --endpoint "${CONFIG_DIR}/endpoints.yaml" --n_jobs $N_JOBS --descriptor rdkit_physchem
-python ${SCRIPT_DIR}/03_calc_descriptors.py --endpoint "${CONFIG_DIR}/endpoints.yaml" --n_jobs $N_JOBS --descriptor mordred
+# python ${SCRIPT_DIR}/03_calc_descriptors.py --endpoint "${CONFIG_DIR}/endpoints.yaml" --n_jobs $N_JOBS --descriptor morgan
+# python ${SCRIPT_DIR}/03_calc_descriptors.py --endpoint "${CONFIG_DIR}/endpoints.yaml" --n_jobs $N_JOBS --descriptor morgan_count
+# python ${SCRIPT_DIR}/03_calc_descriptors.py --endpoint "${CONFIG_DIR}/endpoints.yaml" --n_jobs $N_JOBS --descriptor rdkit_physchem
+# python ${SCRIPT_DIR}/03_calc_descriptors.py --endpoint "${CONFIG_DIR}/endpoints.yaml" --n_jobs $N_JOBS --descriptor mordred
 
 
 # 04 compute embeddings on all data splits and all endpoints
-python ${SCRIPT_DIR}/04_calc_embeddings.py --endpoint "${CONFIG_DIR}/endpoints.yaml" --n_jobs $N_JOBS --model-name chemeleon
-python ${SCRIPT_DIR}/04_calc_embeddings.py --endpoint "${CONFIG_DIR}/endpoints.yaml" --n_jobs $N_JOBS --model-name chemprop
-python ${SCRIPT_DIR}/04_calc_embeddings.py --endpoint "${CONFIG_DIR}/endpoints.yaml" --n_jobs $N_JOBS --model-name chemeleon_no_weights
+CUDA_VISIBLE_DEVICES=0 python ${SCRIPT_DIR}/04_calc_embeddings.py --endpoint "${CONFIG_DIR}/endpoints.yaml" --n_jobs 2 --model-name chemeleon
+CUDA_VISIBLE_DEVICES=0 python ${SCRIPT_DIR}/04_calc_embeddings.py --endpoint "${CONFIG_DIR}/endpoints.yaml" --n_jobs 2 --model-name chemprop
+CUDA_VISIBLE_DEVICES=0 python ${SCRIPT_DIR}/04_calc_embeddings.py --endpoint "${CONFIG_DIR}/endpoints.yaml" --n_jobs 2 --model-name chemeleon_no_pretraining
 
 # 05 KNN probing
 python ${SCRIPT_DIR}/05_knn_probing.py --endpoint "${CONFIG_DIR}/endpoints.yaml" --n_jobs $N_JOBS
