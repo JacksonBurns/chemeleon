a2bee77e5dea037949b7780730a36bd526aecc2b
JacksonBurns <jwburns@mit.edu>
2026-06-09 11:54:17 -0400

    regular chemeleon training but add missing batchnorm

--- models/chemeleon2_preview/evaluate.py
+++ models/chemeleon2_preview/evaluate.py
@@ -154,9 +154,6 @@ timestamp: {datetime.datetime.now()}
             agg = NormAggregation()
             hidden_size = mp.output_dim
 
-            mp.apply(lambda module: module.requires_grad_(False))
-            mp.eval()
-
             # typical chemprop training
             train_idxs, val_idxs = train_test_split(
                 np.arange(len(targets)),
@@ -207,7 +204,7 @@ timestamp: {datetime.datetime.now()}
                     hidden_dim=512,
                 )
             )
-            model = MPNN(mp, agg, fnn)
+            model = MPNN(mp, agg, fnn, batch_norm=True)
 
             _subdir = "".join(c if c.isalnum() else "_" for c in benchmark_name)
             tensorboard_logger = TensorBoardLogger(
@@ -242,41 +239,6 @@ timestamp: {datetime.datetime.now()}
             print(f"Reloading best model from checkpoint file: {ckpt_path}")
             model = MPNN.load_from_checkpoint(ckpt_path)
 
-            # stage two - train the MPNN and the FFN
-            model.message_passing.apply(lambda module: module.requires_grad_(True))
-            model.message_passing.train()
-            tensorboard_logger = TensorBoardLogger(
-                seed_dir / _subdir,
-                name="tensorboard_logs",
-                default_hp_metric=False,
-            )
-            callbacks = [
-                EarlyStopping(
-                    monitor="val_loss",
-                    mode="min",
-                    verbose=False,
-                    patience=5,
-                ),
-                ModelCheckpoint(
-                    monitor="val_loss",
-                    save_top_k=2,
-                    mode="min",
-                    dirpath=seed_dir / _subdir / "checkpoints",
-                ),
-            ]
-            trainer = Trainer(
-                max_epochs=50,
-                logger=tensorboard_logger,
-                log_every_n_steps=1,
-                enable_checkpointing=True,
-                check_val_every_n_epoch=1,
-                callbacks=callbacks,
-            )
-            trainer.fit(model, train_dataloader, val_dataloader)
-            ckpt_path = trainer.checkpoint_callback.best_model_path
-            print(f"Reloading best model from checkpoint file: {ckpt_path}")
-            model = MPNN.load_from_checkpoint(ckpt_path)
-            #
             trainer = Trainer(logger=tensorboard_logger)
             predictions = (
                 torch.vstack(trainer.predict(model, test_dataloader))
